edb32bf09e2bf4c4e7c73aa1d164f8a45f5a0505
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-03-17 16:22:20 +0700

    Menambahkan procedur untuk menampilkan hasil void display()

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -47,11 +47,16 @@ void unsorted() {
     cout << endl;
 }
 
-
-
-
-
-
+void display() {                                            // Procedur untuk menampilkan hasil
+    cout << endl;                                           // Output baris kosong
+    cout << "==================================" << endl;   // Output ke layar
+    cout << "Element Array yang sudah tersusun" << endl;    // Output ke layar
+    cout << "==================================" << endl;   // Output ke layar
+    for (int j = 0; j < n; j++) {                           // Looping dengan j dimulai dari 0 hingga n-1
+        cout << a[j] << endl;                               // Output ke layar
+    }
+    cout << endl;                                           // Output baris kosong
+}
 
 
 int main()
