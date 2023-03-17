d61a76a1a29aeae9fdf81c09532750851855cc76
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-03-17 16:06:16 +0700

    Menambahkan procedur void bubbleSortArray()

--- bubblesort/bubblesort.cpp
+++ bubblesort/bubblesort.cpp
@@ -24,6 +24,17 @@ void input() {              // procedur untuk input
         cin >> a[i];                            // Input dari pengguna
     }
 }
+void bubbleSortArray() {                    // Procedur untuk mengurutkan array dengan metode bubble sort
+    for (int i = 1; i < n; i++) {           // Looping dengan i dimulai dari 1 hingga n-1
+        for (int j = 0; j < n - i; j++) {   // Looping dengan j dimulai dari 0 hingga n-i
+                int temp = a[j];            // Jika nilai pada a[j] lebih besar dari a[j+1]
+            if (a[j] > a[j + 1]) {          // Simpan nilai a[j] ke variabel sementara temp
+                a[j] = a[j + 1];            // Assign nilai a[j+1] ke a[j]
+                a[j + 1] = temp;            // Assign nilai temp ke a[j+1]
+            }
+        }
+    }
+}
 
 
 
